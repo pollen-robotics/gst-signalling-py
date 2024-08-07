01600a8e79c388037099a099ea942168219eacb3
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2024-08-07 16:01:08 +0200

    enhancement #46: add producer test

--- tests/test_list_producers.py
+++ tests/test_list_producers.py
@@ -7,3 +7,7 @@ def test_empty_list(signalling_host: str, signalling_port: int) -> None:
     producers = utils.get_producer_list(signalling_host, signalling_port)
 
     assert len(producers) == 0
+
+
+# @pytest.mark.asyncio
+# async def test_some_asyncio_code(signalling_host: str, signalling_port: int) -> None:
---
+++ tests/test_producer.py
@@ -0,0 +1,16 @@
+import pytest
+
+from gst_signalling import GstSignallingProducer
+
+
+@pytest.mark.asyncio
+async def test_simple_producer(signalling_host: str, signalling_port: int) -> None:
+    producer = GstSignallingProducer(
+        host=signalling_host,
+        port=signalling_port,
+        name="pytest producer",
+    )
+
+    await producer.connect()
+    assert len(producer.peer_id) == 36
+    await producer.close()
